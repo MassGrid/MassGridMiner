c50ed667c32f511f2a83a96c8c18a5d471082c88
wany <wany@ubuntu16.04>
2017-12-21 18:58:35 +0800

    fix writetobinaryfile multicard

--- ocl.c
+++ ocl.c
@@ -498,7 +498,7 @@ bool jh_get_kernel_binary(struct cgpu_info * const cgpu, _clState * const clStat
 	applog(LOG_DEBUG, "%s: Binary size found in binary slot %u: %"PRId64, cgpu->dev_repr, (unsigned)slot, (int64_t)binary_sizes[slot]);
 	if (!binary_sizes[slot])
 		applogr(false, LOG_ERR, "OpenCL compiler generated a zero sized binary, FAIL!");
-	status = clGetProgramInfo(clState->program[algorithmId], CL_PROGRAM_BINARIES, sizeof(binaries), binaries, NULL);
+	status = clGetProgramInfo(clState->program[algorithmId], CL_PROGRAM_BINARIES, sizeof(binaries)*cpnd, binaries, NULL);
 	if (unlikely(status != CL_SUCCESS))
 		applogr(false, LOG_ERR, "Error %d: Getting program info. CL_PROGRAM_BINARIES (clGetProgramInfo)", status);
 	
