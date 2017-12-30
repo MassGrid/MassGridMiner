9504f5401b2cb3a31cc1e0ba74491c14d650d6c6
kyzhao <zhao.kaiyong@gmail.com>
2017-12-31 00:04:49 +0800

    Changes to be committed: 	modified:   ocl.c

--- ocl.c
+++ ocl.c
@@ -715,7 +715,7 @@ err2:
 	cl_context_properties cps[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0 };
 
 	// change clCreateContextFromType to clCreateContext by Kyle
-	// clState->context = clCreateContextFromType(cps, CL_DEVICE_TYPE_GPU, NULL, NULL, &status);
+	//clState->context = clCreateContextFromType(cps, CL_DEVICE_TYPE_GPU, NULL, NULL, &status);
 	clState->context = clCreateContext(cps, 1, &devices[gpu], NULL, NULL, &status);
 	if (status != CL_SUCCESS) {
 		applog(LOG_ERR, "Error %d: Creating Context. (clCreateContextFromType)", status);
